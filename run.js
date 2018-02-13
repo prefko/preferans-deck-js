fd5b58ea28749c30116669c40ec372b40a737222
Predrag Stojadinovic <predrags@nvidia.com>
2018-02-13 17:31:54 +0100

    wip

--- run.js
+++ run.js
@@ -4,7 +4,8 @@
 
 const _ = require('lodash');
 const Deck = require('./lib/deck');
+const Card = require('./lib/deck').Card;
 
-var deck = new Deck();
-var card = new Deck.Card(7, 's');
+// var deck = new Deck();
+var card = new Card(7, 's');
 console.log(card);
