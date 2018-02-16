d9d095e9e4378bab8143db33a8e623fb3e2e0fc3
Predrag Stojadinovic <predrag@stojadinovic.net>
2018-02-16 22:30:09 +0100

    adding some deck methods

--- run.js
+++ run.js
@@ -6,6 +6,8 @@ const _ = require('lodash');
 const Deck = require('./lib/deck');
 const Card = require('./lib/deck').Card;
 
-// var deck = new Deck();
-var card = new Card(7, 's');
-console.log(card);
+var deck = new Deck();
+deck.shuffle();
+console.log(deck.isValid());
+console.log(deck.toString());
+console.log(deck.deal());
