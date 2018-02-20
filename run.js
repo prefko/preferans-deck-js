c23423123d632a7ac2b713b9d01143b275d24c67
Predrag Stojadinovic <predrags@nvidia.com>
2018-02-20 16:53:57 +0100

    mostlz refactoring

--- run.js
+++ run.js
@@ -11,8 +11,7 @@ var deck = new Deck();
 deck.shuffle();
 console.log(deck.getPPN());
 let deal = deck.deal();
-console.log(deal.p1.countSuits(), deal.p1.toString());
-console.log(deal.p2.countSuits(), deal.p2.toString());
-console.log(deal.p3.countSuits(), deal.p3.toString());
-console.log(deal.t.countSuits(), deal.t.toString());
-
+console.log(deal.p1.countSuits() + ': ' + deal.p1.toUnicodeString());
+console.log(deal.p2.countSuits() + ': ' + deal.p2.toUnicodeString());
+console.log(deal.p3.countSuits() + ': ' + deal.p3.toUnicodeString());
+console.log(deal.t.countSuits() + ': ' + deal.t.toUnicodeString());
