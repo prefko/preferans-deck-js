57c4d328fddc1b5d007e89d254d88987b949a472
Predrag Stojadinovic <predrags@nvidia.com>
2018-02-26 16:07:44 +0100

    reducing node version requirement

--- run.js
+++ run.js
@@ -7,13 +7,25 @@ const Deck = require('./lib/deck');
 const Card = require('./lib/deck').Card;
 const Pile = require('./lib/deck').Pile;
 
-var deck = new Deck();
+let deck = new Deck();
 console.log(deck.shuffle().toString());
+
 let deal = deck.deal();
-console.log(deal.p1.sort().toUnicodeString(), '| ', deal.p2.sort().toUnicodeString(), '| ', deal.p3.sort().toUnicodeString(), '| ', deal.t.toUnicodeString());
+console.log(
+	deal.p1.sort().toUnicodeString(), '| ',
+	deal.p2.sort().toUnicodeString(), '| ',
+	deal.p3.sort().toUnicodeString(), '| ',
+	deal.t.toUnicodeString());
 
 console.log('\n');
 
-deck.restore(_.concat(deal.p1.get(), deal.p2.get(), deal.p3.get(), deal.t.get()));
+deck.restore(
+	_.concat(
+		deal.p1.get(),
+		deal.p2.get(),
+		deal.p3.get(),
+		deal.t.get()
+	)
+);
 console.log(deck.toString());
 console.log(deck.shuffle().toString());
